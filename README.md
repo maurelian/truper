47d2dc6bef56bb7b04c457a46c6c834dbb4e9876
maurelian <maurelian@protonmail.ch>
2018-06-11 07:32:46 -0400

    Let it be

---
+++ README.md
@@ -0,0 +1,31 @@
+# Truper
+
+__A simple tool to compile vyper contracts to truffle compatible artifacts.__
+
+This is a somewhat hacky approach to working with vyper in truffle. You need to have the `vyper` compiler installed and available in your terminal's environment. If you can't run `$ vyper -h` this tool will be useless to you. 
+
+This is a bit of a stop-gap, as truffle will likely include this functionality in the future.
+
+## Installation
+
+`npm i -g truper`
+
+## Usage
+
+Compile all `.v.py` files found in `./contracts` or an immediate child directory:
+
+$ `truper`
+
+Compile all `.v.py` files in a specified directory:
+
+$ `truper /path/to/contracts`
+
+The compiled output is always written to `./build/contracts`.
+
+If the contract file name was `myContract.v.py`, the compiler output will be written to `myContract.vyper.json`.
+
+## Importing for use in Truffle tests
+
+`const MyContract = artifacts.require('myContract.vyper');`
+
+The contract can then be used in truffle just as any other solidity contracts compiled by truffle, (though I have not tested with migrations at all).
---
+++ index.js
@@ -0,0 +1,68 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const shell = require('shelljs');
+const colors = require('colors'); // eslint-disable-line
+
+const contractsPath = process.argv[2];
+
+if (contractsPath) {
+  // a path was specified on the command line, build only the vyper files in this dir.
+  build(contractsPath);
+} else {
+  // no path was specified, build all vyper files in contracts dir and immediate child dirs
+  build();
+  const dirs = fs.readdirSync('./contracts').filter(item => fs.statSync(`./contracts/${item}`).isDirectory());
+  for (index in dirs){
+    build(`./contracts/${dirs[index]}`);
+  }
+}
+
+/**
+ * Builds all files in a directory
+ * @param  {string} contractsPath The immediate parent directory of contract files to be compiled
+ * @param  {string} buildPath     The directory to write the output to.
+ */
+function build(contractsPath = './contracts', buildPath = './build/contracts') {
+  // find vyper files:
+  const files = fs.readdirSync(contractsPath);
+  const vyperFiles = files.filter(file => file.slice(-5) == '.v.py');
+
+  // check that the buildPath exists
+  if(!fs.existsSync(buildPath)){
+    throw new Error('The specified build directory does not exists. Default is ./build/contracts')
+  }
+
+  for (const file of vyperFiles) {
+    const filePath = `${contractsPath}/${file}`;
+    buildWithVyper(filePath, buildPath);
+  };
+};
+
+// compiles a single vyper file
+function buildWithVyper(source, buildPath) {
+  console.log(`Compiling vyper from source code: ${source}`.yellow); // eslint-disable-line
+
+  // These property names are chosen to match the Truffle contract schema
+  // https://github.com/trufflesuite/truffle-contract-schema
+  const abi = JSON.parse(shell.exec(`vyper -f json ${source}`, { silent: true })
+    .stdout.replace('\n', ''));
+  const bytecode = shell.exec(`vyper -f bytecode ${source}`, { silent: true })
+    .stdout.replace('\n', '');
+  const deployedBytecode = shell.exec(`vyper -f bytecode_runtime ${source}`, { silent: true })
+    .stdout.replace('\n', '');
+
+  // modify from 'path/to/contract_name.v.py' to 'contract_name'
+  const contractName = source.split('/').pop().split('.')[0];
+
+  const contractDefinition = {
+    contractName, abi, bytecode, deployedBytecode,
+  };
+
+  fs.writeFileSync(
+    `${buildPath}/${contractName}.vyper.json`,
+    JSON.stringify(contractDefinition, null, '  '),
+  );
+  console.log(`Writing output to: ${buildPath}/${contractName}.vyper.json`.green); // eslint-disable-line
+}
+
